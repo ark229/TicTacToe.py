6209c524ece0979853e2e07d878c999532948a30
Arnetta <40047791+ark229@users.noreply.github.com>
2020-06-27 12:56:27 -0400

    Update TicTacToe.py
    added the line draws for horizontal and vertical winners

--- TicTacToe.py
+++ TicTacToe.py
@@ -62,3 +62,52 @@ def draw_status():
     pygame.display.update()
 
 #def check win
+def check_win():
+    global TTT, winner, draw
+    #check for winning rows
+    for row in range(0, 3):
+        if ((TTT [row][0] == TTT [row][1] == TTT[row][2]) and (TTT [row][0] is not None)):
+            #this row won
+            winner = TTT[row][0]
+            pygame.draw.line(screen, (250,0,0), (0, (row + 1) *height/3 -height/6),\ (width, (row + 1) *height/3 -height/6), 4)
+            break
+
+    #check for winning columns
+    for col in range(0, 3):
+        if (TTT[0] [col] == TTT[1] [col] == TTT[2] [col]) and (TTT[0] [col] is not None):
+            #this column won
+            winner = TTT[0][col]
+            #draw winning line
+            pygame.draw.line(screen, (250, 0, 0), ((col + 1)* width/3 - width/6, 0), \ ((col + 1) * width/3 -width/6, height), 4)
+            break
+    #check for diagonal winners
+    if (TTT [0][0] == TTT[1][1] == TTT[2][2])and (TTT[0][0] is not None):
+        #game won diagonally left to right
+        winner = TTT[0][2]
+        pygame.draw.line(screen, (250, 70, 70), (350, 50), (50, 350), 4)
+
+    if (all([all(row) for row in TTT]) and winner is None):
+        draw = True
+    draw_status()
+
+def drawXO(row, col):
+    global TTT, XO
+    if row == 1:
+        posx = 30
+    if row == 2:
+        posx = width/3 + 30
+    if row == 3:
+        posx = width/3*2 + 30
+
+    if col == 1:
+        posy = 30
+    if col == 2:
+        posy = height/3 + 30
+    if col == 3:
+        posy = height/3*2 + 30
+    TTT [row - 1][col - 1] = XO
+        if(XO == 'x'):
+            screen.blit(x_img, (posy, posx))
+    XO = 'o'
+        else:
+            screen.blit(o_img), (posy, posx))
